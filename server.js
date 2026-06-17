3402364487d083bb311979ffabfe64666febea86
Jose Rodriguez <tellojose675@gmail.com>
2026-06-17 11:52:29 -0500

    Fix static files path for Vercel

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // =========================
 // POSTGRESQL
